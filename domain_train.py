bug fix for reverse gradient layer

domain_train.py
@@ -236,8 +236,6 @@ def trainModel(model, trainData, validData, domain_train, domain_valid, dataset,
         for i in range(len(trainData)):
 
             batchIdx = batchOrder[i] if epoch > opt.curriculum else i
-            # TODO Remove this
-            batchIdx = 312
             batch = trainData[batchIdx][:-1] # exclude original indices
 
             model.zero_grad()
@@ -257,14 +255,10 @@ def trainModel(model, trainData, validData, domain_train, domain_valid, dataset,
                 discrim_correct, num_discrim_elements = get_accuracy(torch.cat([old_domain, new_domain]).data.squeeze(), discriminator_targets.data)
 
 
-                print "old_domain: ", old_domain.size()
-                print "new_domain: ", new_domain.size()
-                print "discriminator_targets: ", discriminator_targets.size()
                 discriminator_loss = discriminator_criterion(torch.cat([old_domain, new_domain]), discriminator_targets)
             else:
                 outputs = model(batch)
 
-            print batchIdx, batchIdxAdapt
             targets = batch[1][1:]  # exclude <s> from targets
             loss, gradOutput, num_correct = memoryEfficientLoss(
                     outputs, targets, model.generator, criterion)
@@ -395,10 +389,9 @@ def main():
         nn.LogSoftmax())
 
     if opt.adapt:
-        discriminator    = Discriminator(opt.word_vec_size  * opt.layers)
-        reverse_gradient = ReverseGradient()
+        discriminator = Discriminator(opt.word_vec_size  * opt.layers)
 
-    model = onmt.DomainModels.NMTModel(encoder, decoder, discriminator, reverse_gradient)
+    model = onmt.DomainModels.NMTModel(encoder, decoder, discriminator)
 
     if opt.train_from:
         print('Loading model from checkpoint at %s' % opt.train_from)

onmt/DomainModels.py
@@ -5,6 +5,7 @@ from torch.autograd import Variable
 import onmt.modules
 from torch.nn.utils.rnn import pad_packed_sequence as unpack
 from torch.nn.utils.rnn import pack_padded_sequence as pack
+from onmt.modules.reverse_gradient import ReverseGradient
 
 class Encoder(nn.Module):
 
@@ -116,12 +117,11 @@ class Decoder(nn.Module):
 
 class NMTModel(nn.Module):
 
-    def __init__(self, encoder, decoder, discriminator=None, reverse_gradient=None):
+    def __init__(self, encoder, decoder, discriminator=None, reverse_gradient=None, reverse_gradient_2=None):
         super(NMTModel, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
         self.discriminator = discriminator
-        self.reverse_gradient = reverse_gradient
 
     def make_init_decoder_output(self, context):
         batch_size = context.size(1)
@@ -184,24 +184,12 @@ class NMTModel(nn.Module):
 
 
             # TODO: training flag, and maybe concatenate the two batches!?
-            before = ((enc_hidden[1].transpose(0,1).contiguous().view(old_batch_size,-1)))
-            print 'before: ', before.size()
-            after  = (self.reverse_gradient(enc_hidden[1].transpose(0,1).contiguous().view(old_batch_size,-1)))
-            relu_after  = (F.relu(enc_hidden[1].transpose(0,1).contiguous().view(old_batch_size,-1)))
-            print 'after: ', after.size()
-            print 'relu after: ', relu_after.size()
 #            old_domain = self.discriminator(F.relu(enc_hidden[1].transpose(0,1).contiguous().view(old_batch_size,-1)))
-            old_domain = self.discriminator(self.reverse_gradient(enc_hidden[1].transpose(0,1).contiguous().view(old_batch_size,-1)))
+            old_domain = self.discriminator(ReverseGradient()(enc_hidden[1].transpose(0,1).contiguous().view(old_batch_size,-1)))
 
             # This should give a label of 0
-            before = ((enc_hidden_adapt[1].transpose(0,1).contiguous().view(new_batch_size,-1)))
-            after = (self.reverse_gradient(enc_hidden_adapt[1].transpose(0,1).contiguous().view(new_batch_size,-1)))
-            relu_after = (F.relu(enc_hidden_adapt[1].transpose(0,1).contiguous().view(new_batch_size,-1)))
-            print 'before: ', before.size()
-            print 'after: ', after.size()
-            print 'relu after: ', relu_after.size()
 #            new_domain = self.discriminator(F.relu(enc_hidden_adapt[1].transpose(0,1).contiguous().view(new_batch_size,-1)))
-            new_domain = self.discriminator(self.reverse_gradient(enc_hidden_adapt[1].transpose(0,1).contiguous().view(new_batch_size,-1)))
+            new_domain = self.discriminator(ReverseGradient()(enc_hidden_adapt[1].transpose(0,1).contiguous().view(new_batch_size,-1)))
 
             return out, old_domain, new_domain
 

onmt/modules/reverse_gradient.py
@@ -1,44 +1,10 @@
 import torch
 
-
-# Code in file autograd/two_layer_net_custom_function.py
-import torch
-from torch.autograd import Variable
-
 class ReverseGradient(torch.autograd.Function):
-  """
-  We can implement our own custom autograd Functions by subclassing
-  torch.autograd.Function and implementing the forward and backward passes
-  which operate on Tensors.
-  """
-  def forward(self, input):
-    """
-    In the forward pass we receive a Tensor containing the input and return a
-    Tensor containing the output. You can cache arbitrary Tensors for use in the
-    backward pass using the save_for_backward method.
-    """
-    self.save_for_backward(input)
-    return input.clamp(min=0)
-
-  def backward(self, grad_output):
-    """
-    In the backward pass we receive a Tensor containing the gradient of the loss
-    with respect to the output, and we need to compute the gradient of the loss
-    with respect to the input.
-    """
-    input, = self.saved_tensors
-    grad_input = grad_output.clone()
-    grad_input[input < 0] = 0
-    return grad_input
-
-
-
-#class ReverseGradient(torch.nn.functional):
-#    def __inti__(self):
-#        super(ReverseGradient, self).__init__()
-#    def forward(self, x):
-#        return x
-#    def backward(self, x):
-#        print 'x: ', x.size()
-##        return -x
-#        return x
+    def __inti__(self):
+        super(ReverseGradient, self).__init__()
+    def forward(self, x):
+        return x
+    def backward(self, x):
+#        return -x
+        return x