b63be94d383a90aa55c84416c5719c226c82bcdd
Amr Sharaf <amr@umiacs.umd.edu>
2017-04-17 21:07:36 -0400

    the gradient reversing layer

---
+++ onmt/modules/reverse_gradient.py
@@ -0,0 +1,9 @@
+import torch
+
+class ReverseGradient(torch.autograd.Function):
+    def __inti__(self):
+        super(ReverseGradient, self).__init__()
+    def forward(self, x):
+        return x
+    def backward(self, x):
+        return -x
