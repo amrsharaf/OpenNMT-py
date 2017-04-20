1ce3a6730872c5970296ff922b5433d7d7bb8feb
Amr Sharaf <amr@umiacs.umd.edu>
2017-04-20 18:42:44 -0400

    Discriminator cleanup

--- onmt/modules/discriminator.py
+++ onmt/modules/discriminator.py
@@ -5,18 +5,8 @@ class Discriminator(nn.Module):
     def __init__(self, input_size):
         super(Discriminator, self).__init__()
         self.lin1 = nn.Linear(input_size, 1)
-#        self.lin1 = nn.Linear(input_size, input_size * 4)
-        self.lin2 = nn.Linear(input_size * 4, input_size * 4)
-        self.lin3 = nn.Linear(input_size * 4, 1)
 
     def forward(self, x):
         output = self.lin1(x)
- #       output = F.relu(output)
-        # TODO: Dropout test / train?
-#        output = F.dropout(output, p=0.5, training=training)
-#        output = self.lin2(output)
-#        output = F.relu(output)
-#        output = F.dropout(output, p=0.5, training=training)
-#        output = self.lin3(output)
         output = F.sigmoid(output)
         return output
